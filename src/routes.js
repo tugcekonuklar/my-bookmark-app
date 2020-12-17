99b801c1030aae5b9b1009a143e886d0f2589f43
tugcekonuklar <tkonuklar@gmail.com>
2020-12-17 04:05:12 +0100

    fix: routing problem solved by define Outlet

--- src/routes.js
+++ src/routes.js
@@ -2,19 +2,31 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import Home from './pages/home/Home';
 import AddBookMark from './pages/add-bookmark/AddBookMark'
+import MainLayout from './layouts/main-layout/MainLayout'
+import NotFoundView from './errors/NotFoundView'
 
 const routes = [
+  // {
+  //   path: 'app/dashboard',
+  //   element: <Home />,
+  //   children: [
+  //     { path: 'addBookmark', element:  <Navigate to={<AddBookMark />}/> },
+  //     { path: '*', element: <Navigate to="/404" /> }
+  //   ]
+  // },
+  // {
+  //   path: 'app/add-bookmark',
+  //   element: <AddBookMark />
+  // },
   {
-    path: 'app',
-    element: <Home />,
+    path: '/',
+    element: <MainLayout />,
     children: [
-      { path: 'addBookmark', element: <AddBookMark /> },
+      { path: '404', element: <NotFoundView /> },
+      { path: '/', element: <Home /> },
+      { path: 'add-bookmark', element: <AddBookMark /> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
-  },
-  {
-    path: 'app/add-bookmark',
-    element: <AddBookMark />
   }
 ];
 
